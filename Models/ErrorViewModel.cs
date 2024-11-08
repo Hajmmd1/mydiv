a62049fd9e14954ec381cbd782c141636b859a4e
Hajmmd1 <m1378d78@gmail.com>
2024-11-08 10:49:55 +0330

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mydiv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
